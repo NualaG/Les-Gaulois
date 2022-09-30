5a1eb39e99401c8babe65b29b781f27427e26191
NualaG <114426978+NualaG@users.noreply.github.com>
2022-09-30 20:10:45 +0200

    3

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -4,6 +4,16 @@ public class Scenario {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		personnages.Gaulois asterix = new personnages.Gaulois("Astérix", 8);
+		personnages.Romain minus = new personnages.Romain("Minus", 6);
+		
+		asterix.parler("Bonjour à tous");
+		minus.parler("UN GAU... UN GAUGAU...");
+		
+		asterix.frapper(minus);
+		asterix.frapper(minus);
+		asterix.frapper(minus);
+		
 
 	}
 
